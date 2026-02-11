0fef49b1426d95c7ce397ffe58874a50e6fe01ad
Pilar Mantilla <mdmantillag@gmail.com>
2026-02-11 16:19:43 -0500

    inicializa spring boot con gradle

---
+++ mvc-estudiantes/src/main/java/com/example/mvc_estudiantes/MvcEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvc_estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcEstudiantesApplication.class, args);
+	}
+
+}
---
+++ mvc-estudiantes/src/test/java/com/example/mvc_estudiantes/MvcEstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvc_estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcEstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
